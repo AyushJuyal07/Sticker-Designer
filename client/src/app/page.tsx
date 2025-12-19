d6f02e8a7110265b40cee816c0ab241147084c46
Ayush Juyal <ayushjuyal26@gmail.com>
2025-12-20 00:29:08 +0530

    minor changes for deployment

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center">
-      <a
-        href="/designer"
-        className="rounded-xl bg-black px-6 py-3 text-white"
-      >
-        Start Designing
-      </a>
-    </main>
-  )
+  redirect("/designer")
 }
